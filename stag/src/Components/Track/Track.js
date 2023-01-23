84784ab74861b4f6f923a0c2712631ec688b34a5
Georgeoak03 <101403429+Georgeoak03@users.noreply.github.com>
2023-01-23 13:39:31 +0000

    added search passdown and render functionality

--- stag/src/Components/Track/Track.js
+++ stag/src/Components/Track/Track.js
@@ -16,8 +16,8 @@ class Track extends React.Component {
         return (
             <div className="Track">
                 <div className="Track-information">
-                    <h3>track name</h3>
-                    <p>track artist will go here | track album will go here</p>
+                    <h3>{this.props.track.name}</h3>
+                    <p>{this.props.track.artist} | {this.props.track.album}</p>
                 </div>
                 {this.renderAction()}
             </div>
