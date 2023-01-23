fea7ddf7638500a58628ac5eddb8a19aaeeac20b
Georgeoak03 <101403429+Georgeoak03@users.noreply.github.com>
2023-01-23 17:47:00 +0000

    added change playlist name functionality

--- stag/src/Components/App/App.js
+++ stag/src/Components/App/App.js
@@ -20,6 +20,7 @@ class App extends React.Component {
         }
         this.addTrack = this.addTrack.bind(this)
         this.removeTrack = this.removeTrack.bind(this)
+        this.updatePlaylistName = this.updatePlaylistName.bind(this)
     }
 
     addTrack(track) {
@@ -39,6 +40,10 @@ class App extends React.Component {
         this.setState({ playlistTracks: tracks })
     }
 
+    updatePlaylistName(name) {
+        this.setState({ playlistName: name })
+    }
+
     render() {
         return (
             <div>
@@ -51,7 +56,8 @@ class App extends React.Component {
                         />
                         <Playlist playlistName={this.state.playlistName}
                                   playlistTracks={this.state.playlistTracks}
-                                  onRemove={this.removeTrack}/>
+                                  onRemove={this.removeTrack}
+                                onNameChange={this.updatePlaylistName}/>
                     </div>
                 </div>
             </div>
