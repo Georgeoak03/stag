9b04a6bab7c31c595cf7a338848d9e6019e8f83f
Georgeoak03 <101403429+Georgeoak03@users.noreply.github.com>
2023-01-24 16:03:11 +0000

    site works and saves to spotify playlist on user account

--- stag/src/Components/TrackList/TrackList.js
+++ stag/src/Components/TrackList/TrackList.js
@@ -8,12 +8,13 @@ class TrackList extends React.Component {
         return (
             <div className="TrackList">
                 {
-                    this.props.tracks.map(track => {
-                        return <Track track={track}
+                    this.props.tracks.map((track) => {
+                        return (<Track track={track}
                                     key={track.id}
                                     onAdd={this.props.onAdd}
                                     onRemove={this.props.onRemove}
                                     isRemoval={this.props.isRemoval}/>
+                        )
                     })
                 }
             </div>
